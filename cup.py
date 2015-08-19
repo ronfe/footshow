f724d4c493ef765b53ba9ff7bae5d3933327f518
ronfe <chihungfei@126.com>
2015-08-19 17:33:15 +0800

    update cup

--- cup.py
+++ cup.py
@@ -1,5 +1,42 @@
 __author__ = 'ronfe'
 from main import probTable, match
+import string
+
+def updateLeague(league, team, updateA):
+    f = open('teams/1516' + league + '.csv').readlines()
+    # convert to dict
+    leagueDict = {}
+    for each in f:
+        if '$home' not in each:
+            tempTeam, attack, defence = each.strip().split(',')
+            attack = string.atof(attack)
+            defence = string.atof(defence)
+            leagueDict[tempTeam] = [attack, defence]
+    # calc the rate
+    matchTeam = leagueDict[team]
+    attackRate = updateA[0] / matchTeam[0]
+    defendRate = updateA[1] / matchTeam[1]
+
+    # update the league
+    for each in leagueDict:
+        originData = leagueDict[each]
+        newData = [originData[0] * attackRate, originData[1] * defendRate]
+        leagueDict[each] = newData
+
+    # record
+    g = open('teams/1516' + league + '.csv', 'w')
+    for each in leagueDict:
+        tempString = each + ',' + str(leagueDict[each][0]) + str(leagueDict[each][1]) + '\n'
+        g.write(tempString)
+
+    g.close()
+
+
+
+
 
 data = probTable(0.9257872,0.726743231386,0.887162603291,0.57925995,0.3306335)
 a = match(data, 2, 1, [0.9257872,0.726743231386,0.887162603291,0.57925995])
+updateLeague('por1', 'Sporting CP', a[0])
+updateLeague('rus1', 'CSKA Moscow', a[1])
+print 'done'
